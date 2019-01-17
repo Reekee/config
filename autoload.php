1f3e6a8bf8c1fd4d0f8a609cb7038b9a22e5dc62
Reekee <r_kee19384@hotmail.com>
2019-01-17 22:05:36 +0700

    save

--- autoload.php
+++ autoload.php
@@ -2,9 +2,9 @@
 	header('Access-Control-Allow-Origin: *');
 	include_once("class.database.php");
 	define("host", "localhost");
-    define("user", "root");
-    define("pass", "");
-    define("dbname", "db_touristapp");
+	define("user", "root");
+	define("pass", "");
+	define("dbname", "db_touristapp");
 	$DATABASE = new Database(host, user, pass, dbname);
 	$POSTDATA = file_get_contents("php://input");
-    $REQUEST = json_decode($POSTDATA);
+	$REQUEST = json_decode($POSTDATA);
